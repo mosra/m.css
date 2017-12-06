1db552749a0345da564876fec77ab3ca3f384eff
Vladimír Vondruš <mosra@centrum.cz>
2017-12-06 23:47:51 +0100

    doxygen: add a test for add_wbr().
    Doesn't do the right thing at all times. That's why I'm adding it, ha.

---
+++ doxygen/test/test_utility.py
@@ -0,0 +1,13 @@
+import unittest
+
+from dox2html5 import add_wbr
+
+class Utility(unittest.TestCase):
+    def test_add_wbr(self):
+        self.assertEqual(add_wbr('Corrade::Containers'), 'Corrade::<wbr/>Containers')
+        self.assertEqual(add_wbr('CORRADE_TEST_MAIN()'), 'CORRADE_<wbr/>TEST_<wbr/>MAIN()')
+        self.assertEqual(add_wbr('http://magnum.graphics/showcase/'), 'http:/<wbr/>/<wbr/>magnum.graphics/<wbr/>showcase/<wbr/>')
+        self.assertEqual(add_wbr('<strong>a</strong>'), '<strong>a</strong>')
+
+if __name__ == '__main__':
+    unittest.main()
