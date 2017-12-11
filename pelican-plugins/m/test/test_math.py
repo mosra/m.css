8bb27d2d9d58302f378313c9108e76d58d307854
Vladimír Vondruš <mosra@centrum.cz>
2017-12-11 15:24:52 +0100

    m.math: test only on Python 3.5+.

--- pelican-plugins/m/test/test_math.py
+++ pelican-plugins/m/test/test_math.py
@@ -1,9 +1,16 @@
+import sys
+import unittest
+
+from distutils.version import LooseVersion
+
 from m.test import PluginTestCase
 
 class Math(PluginTestCase):
     def __init__(self, *args, **kwargs):
         super().__init__(__file__, '', *args, **kwargs)
 
+    @unittest.skipUnless(LooseVersion(sys.version) >= LooseVersion("3.5"),
+                         "The math plugin requires at least Python 3.5")
     def test(self):
         self.run_pelican({
             'PLUGINS': ['m.htmlsanity', 'm.math']
