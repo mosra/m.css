d26d426d536b35fa46b6475f7472ed4655688205
Vladimír Vondruš <mosra@centrum.cz>
2017-12-21 18:56:54 +0100

    site: no need for a leading slash.

--- site/pelicanconf.py
+++ site/pelicanconf.py
@@ -144,11 +144,11 @@ THEME = '../pelican-theme'
 THEME_STATIC_DIR = 'static'
 M_THEME_COLOR = '#22272e'
 M_CSS_FILES = ['https://fonts.googleapis.com/css?family=Source+Code+Pro:400,400i,600%7CSource+Sans+Pro:400,400i,600,600i&subset=latin-ext',
-               '/static/m-dark.css',
-              #'/static/m-debug.css'
+               'static/m-dark.css',
+              #'static/m-debug.css'
               ]
 #M_CSS_FILES = ['https://fonts.googleapis.com/css?family=Libre+Baskerville:400,400i,700,700i%7CSource+Code+Pro:400,400i,600',
-               #'/static/m-light.css']
+               #'static/m-light.css']
 
 FORMATTED_FIELDS = ['summary', 'landing', 'header', 'footer', 'description', 'badge']
 
