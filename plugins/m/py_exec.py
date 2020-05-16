5d3d3b5ab0d6470eb4e6c2e42439f9acefe83fb4
Sergei Izmailov <sergei.a.izmailov@gmail.com>
2020-05-17 02:55:39 +0300

    Add `py-exec` directive to execute python snippets
    Directive effectively runs `exec(<snippet code>)` and
    renders highlighted code & output (if any)
    
    Depends on `m.code` plugin
    
    Features:
      - Allowed to created "contexts" to preserve state
        between snippets
      - code/stdout/stderr render can be optionally suppressed
      - top-level exceptions treated as errors
      - snippetes with expected exceptions must be
        explicitly annotated with :raises: ExpectedExceptionType

---
+++ plugins/m/py_exec.py
@@ -0,0 +1,143 @@
+#
+#   This file is part of m.css.
+#
+#   Copyright © 2017, 2018, 2019 Vladimír Vondruš <mosra@centrum.cz>
+#   Copyright © 2020 Sergei Izmailov <sergei.a.izmailov@gmail.com>
+#
+#   Permission is hereby granted, free of charge, to any person obtaining a
+#   copy of this software and associated documentation files (the "Software"),
+#   to deal in the Software without restriction, including without limitation
+#   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#   and/or sell copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included
+#   in all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#   DEALINGS IN THE SOFTWARE.
+#
+
+import io
+import traceback
+
+from contextlib import redirect_stdout, redirect_stderr
+from docutils.parsers import rst
+from docutils.parsers.rst.roles import set_classes
+from docutils.parsers.rst import Directive, directives
+from docutils import nodes
+
+from m.code import _highlight
+
+
+def strip_second_line(text):
+    l1_pos = text.find('\n') + 1
+    l2_pos = text.find('\n', l1_pos) + 1
+    l3_pos = text.find('\n', l2_pos) + 1
+    return text[:l1_pos] + text[l3_pos:]
+
+
+_exec_contexts = {}
+
+
+class PyCodeExec(Directive):
+    required_arguments = 0
+    optional_arguments = 0
+    final_argument_whitespace = True
+    option_spec = {
+        'hl_lines': directives.unchanged,    # same as in m.code.Code
+        'class': directives.class_option,    # same as in m.code.Code
+        'filters': directives.unchanged,     # same as in m.code.Code
+        'context-id': directives.unchanged,  # serves to share state between snippets
+        'raises': directives.unchanged,      # expected exception of the snippet (just class name)
+        'hide-code': directives.flag,        # suppress snippet code
+        'hide-stdout': directives.flag,      # suppress snippet stdout
+        'hide-stderr': directives.flag,      # suppress snippet stderr
+        'discard-context': directives.flag,  # don't forget to clean-up named contexts
+    }
+    has_content = True
+
+    def run(self):
+        self.assert_has_content()
+
+        set_classes(self.options)
+        classes = []
+        if 'classes' in self.options:
+            classes += self.options['classes']
+            del self.options['classes']
+
+        filters = self.options.pop('filters', '').split()
+
+        gl = {}
+
+        if 'context-id' in self.options:
+            ctx_id = self.options['context-id']
+            if ctx_id in _exec_contexts:
+                gl = _exec_contexts[ctx_id]
+            else:
+                _exec_contexts[ctx_id] = gl
+
+            if 'discard-context' in self.options:
+                del _exec_contexts[ctx_id]
+
+        stdout = io.StringIO()
+        stderr = io.StringIO()
+        try:
+            with redirect_stdout(stdout):
+                with redirect_stderr(stderr):
+                    exec("\n".join(self.content), gl)
+        except Exception as exc:
+            exc_class_name = exc.__class__.__name__
+            if 'raises' not in self.options:
+                raise self.severe('Snippet raised exception\n'
+                                  'Add `:raises: <exception-type>` to show exceptional snippet\n'.format(self.name) +
+                                  strip_second_line(traceback.format_exc()))
+            if exc_class_name != self.options['raises']:
+                raise self.severe('Snippet raised unexpected exception {}, \n'.format(exc_class_name) +
+                                  'Expected exception type: {}\n'.format(self.options['raises']) +
+                                  strip_second_line(traceback.format_exc())
+                                  )
+            stderr.write(strip_second_line(traceback.format_exc()))
+        else:
+            if 'raises' in self.options:
+                raise self.severe('Snippet was expected to raise {} exception '.format(self.options['raises']) +
+                                  'but it didn\'t')
+        finally:
+            stdout = stdout.getvalue()
+            stderr = stderr.getvalue()
+
+        result = []
+
+        if 'hide-code' not in self.options:
+            result += self._run("\n".join(self.content), 'py', filters, classes)
+        if 'hide-stdout' not in self.options and stdout:
+            result += self._run(stdout, 'ansi', filters, classes)
+        if 'hide-stderr' not in self.options and stderr:
+            result += self._run(stderr, 'ansi', filters, classes)
+
+        return result
+
+    def _run(self, content: object, lang: object, filters: object, classes: object) -> object:
+
+        class_, highlighted = _highlight(content, lang, self.options, is_block=True, filters=filters)
+        classes += [class_]
+
+        content = nodes.raw('', highlighted, format='html')
+        pre = nodes.literal_block('', classes=classes)
+        pre.append(content)
+
+        return [pre]
+
+
+def register_mcss(**kwargs):
+    rst.directives.register_directive('py-exec', PyCodeExec)
+
+# Below is only Pelican-specific functionality. If Pelican is not found, these
+# do nothing.
+
+register = register_mcss
---
+++ plugins/m/test/py_exec/page.rst
@@ -0,0 +1,99 @@
+m.py_exec
+#########
+
+First context, no output:
+
+.. py-exec::
+    :context-id: ctx1
+
+    from typing import List
+    a = 12
+
+Second context, no output:
+
+.. py-exec::
+    :context-id: ctx2
+
+    List = [1,2,3]
+
+Anon context, expect top-level RuntimeError:
+
+.. py-exec::
+    :raises: RuntimeError
+
+    def f():
+        raise RuntimeError()
+    f()
+
+First context, print to stdout:
+
+.. py-exec::
+    :context-id: ctx1
+
+    print(a + 2)
+
+Second context:
+
+.. py-exec::
+    :context-id: ctx2
+
+    assert List + [4] == [1,2,3,4]
+
+Anon context, suppressed output:
+
+.. py-exec::
+    :hide-stdout:
+
+    print(4 + 2 / 2)
+
+Anon context, suppressed stderr:
+
+.. py-exec::
+    :hide-stderr:
+
+    import sys
+    print(4 + 2 / 2, file=sys.stderr)
+
+
+First context, print and destroy context:
+
+.. py-exec::
+    :context-id: ctx1
+    :discard-context:
+
+    print(List)
+
+Second context, print and destroy context:
+
+.. py-exec::
+    :context-id: ctx2
+    :discard-context:
+
+    print(List)
+
+Both contexts are destroyed now, variable access results to expected NameError:
+
+.. py-exec::
+    :context-id: ctx1
+    :raises: NameError
+    :discard-context:
+
+    print(List)
+
+
+.. py-exec::
+    :context-id: ctx2
+    :raises: NameError
+    :discard-context:
+
+    print(List)
+
+List comprehension:
+
+.. py-exec::
+
+    def foo(a):
+        return a * 2 + 2
+    print([ foo(i) for i in [1,2] ])
+
+
