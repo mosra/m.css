d1837841fc4705f311a106c2efe553f811c46a9d
Vladimír Vondruš <mosra@centrum.cz>
2020-06-08 21:35:29 +0200

    m.code: remove the ZWNJ hack.
    Was used only to fix one browser rendering issue on magnum.graphics
    which happened only when Unicode blocks were involved. Does not make
    sense to pollute all output with it.

--- plugins/ansilexer.py
+++ plugins/ansilexer.py
@@ -89,8 +89,8 @@ class AnsiLexer(RegexLexer):
         ]
     }
 
-_ansi_fg_color_re = re.compile('class="g g-AnsiForegroundColor([0-9a-f]{6})">')
-_ansi_fg_bg_color_re = re.compile('class="g g-AnsiForegroundBackgroundColor([0-9a-f]{6})">')
+_ansi_fg_color_re = re.compile('class="g g-AnsiForegroundColor([0-9a-f]{6})"')
+_ansi_fg_bg_color_re = re.compile('class="g g-AnsiForegroundBackgroundColor([0-9a-f]{6})"')
 
 class HtmlAnsiFormatter(HtmlFormatter):
     def wrap(self, source, outfile):
@@ -99,12 +99,7 @@ class HtmlAnsiFormatter(HtmlFormatter):
     def _wrap_code(self, source):
         for i, t in source:
             if i == 1: # it's a line of formatted code
-                # Add ZWNJ for before each character because otherwise it's
-                # somehow impossible to wrap even with word-break: break-all.
-                # Not sure why (and not sure if this is the best solution), but
-                # had to ship a thing so there it is. Adding <wbr/> did not
-                # help.
-                t = _ansi_fg_bg_color_re.sub('style="color: #\\1; background-color: #\\1">&zwnj;', t)
-                t = _ansi_fg_color_re.sub('style="color: #\\1">&zwnj;', t)
+                t = _ansi_fg_bg_color_re.sub('style="color: #\\1; background-color: #\\1"', t)
+                t = _ansi_fg_color_re.sub('style="color: #\\1"', t)
                 #t += 'H'
             yield i, t
