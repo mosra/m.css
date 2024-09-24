97ce2a18305ecafd63e38538c301fbfe6ffa1b3b
Vladimír Vondruš <mosra@centrum.cz>
2024-09-24 19:04:24 +0200

    plugins: add a test for parse_version().
    Forgot to add this file in 8f080819783f2daf085d8b8d954dcbee4d27c2a9.

---
+++ plugins/m/test/test.py
@@ -0,0 +1,38 @@
+#
+#   This file is part of m.css.
+#
+#   Copyright © 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024
+#             Vladimír Vondruš <mosra@centrum.cz>
+#
+#   Permission is hereby granted, free of charge, to any person obtaining a
+#   copy of this software and associated documentation files (the "Software"),
+#   to deal in the Software without restriction, including without limitation
+#   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+#   and/or sell copies of the Software, and to permit persons to whom the
+#   Software is furnished to do so, subject to the following conditions:
+#
+#   The above copyright notice and this permission notice shall be included
+#   in all copies or substantial portions of the Software.
+#
+#   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+#   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+#   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+#   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+#   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+#   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+#   DEALINGS IN THE SOFTWARE.
+#
+
+import unittest
+
+from . import parse_version
+
+# Tests the parse_version utility, just to be sure it's not giving some
+# surprises compared to docutils.version.LooseVersion
+
+class ParseVersion(unittest.TestCase):
+    def test(self):
+        self.assertEqual(parse_version("3.5.71"), (3, 5, 71))
+        self.assertLess(parse_version("4"), (5, ))
+        self.assertLess(parse_version("4"), (4, 1))
+        self.assertLess(parse_version("4.1"), (5, ))
